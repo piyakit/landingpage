a2f203cd35e61cf7c8c0db9e7a56a1c168842479
piyakit <piyakit.tho@gmail.com>
2024-09-24 14:17:36 +0700

    Add set cookie

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import './App.css'
 import Banner from './components/Banner'
 import Footer from './components/Footer'
@@ -6,8 +7,21 @@ import Navbar from './components/Navbar'
 import Solution from './components/Solution'
 import SponsorLogo from './components/SponsorLogo'
 import Testimonial from './components/Testimonial'
+import Cookies from 'js-cookie';
 
 function App() {
+
+  const setLanguageCookie = (language: string) => {
+    Cookies.set('language', language);
+  }
+
+  const setCookie = () => {
+    setLanguageCookie('th')
+  }
+
+  useEffect(()=>{
+    setCookie()
+  }, [])
 
   return (
     <div className='mx-20'>
